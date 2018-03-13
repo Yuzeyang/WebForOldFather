cc5b8e95bebe99b33c62e6defc306cbbdb059060
Yuzeyang <ios.yuzeyang@gmail.com>
2018-03-13 08:01:57 +0800

    初写前端路漫漫

--- WebForOldFather/src/App.vue
+++ WebForOldFather/src/App.vue
@@ -1,7 +1,19 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <div id="banner">
+      <div>
+        <img src="" alt="">
+        <label for="">绍兴圣御山酒业</label>
+      </div>
+      <div id="tab">
+        <button id="tab-button">首页</button>
+        <button id="tab-button">走进企业</button>
+        <button id="tab-button">品牌文化</button>
+        <button id="tab-button">产品展示</button>
+        <button id="tab-button">黄酒文化</button>
+        <button id="tab-button">联系我们</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,5 +31,22 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#banner {
+  height: 50px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+#tab {
+  width: 500px;
+  display: flex;
+  justify-content: space-around;
+}
+
+#tab-button {
+  border-style: none;
 }
 </style>
